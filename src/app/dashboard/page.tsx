3eaa37f62411a641b7a542d92e13d66c826724a9
Leandro Hosken <lshv04@gmail.com>
2025-02-06 10:44:09 -0300

    working on deploy

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,24 +1,25 @@
 // pages/dashboard.tsx
-import type { NextPage } from 'next';
-import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server';
-
-
+import type { NextPage } from "next";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 
 const Dashboard: NextPage = async () => {
-
-  const {getUser} = getKindeServerSession();
-  const user =  await getUser();
-  
+  const { getUser, isAuthenticated } = getKindeServerSession();
+  const user = await getUser();
+  const isUserAuthenticated = await isAuthenticated();
 
   return (
     <main className="container mx-auto p-4">
       <h1 className="text-2xl font-bold mb-4">Dashboard</h1>
-      <h2>Informações do usuário:</h2>
-      <p> Email: {user.email}</p>
-      <p> Name: {user.given_name} {user.family_name} </p>
-      
-
-
+      {isUserAuthenticated && (
+        <div>
+          <h2>Informações do usuário:</h2>
+          <p> Email: {user.email}</p>
+          <p>
+            {" "}
+            Name: {user.given_name} {user.family_name}{" "}
+          </p>
+        </div>
+      )}
     </main>
   );
 };
